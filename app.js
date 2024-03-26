898b26bccfaf859688b66db0203aa0d07b32d497
Makis Chartalos <makischartalos@gmail.com>
2024-03-26 12:09:38 +0200

    Swagger Documentation

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 
 app.use(express.json());
 
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger');
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocument = require()
+
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(
@@ -15,15 +16,14 @@ mongoose.connect(process.env.MONGODB_URI)
   );
 
 const user = require('./routes/user.route');
-// const userProduct = require('./routes/user.products.routes')
 
-app.use('/api/users', user)
-// app.use('/api/user-products', userProduct)
+app.use('/api/users', user) 
+app.useProduct = require('./routes/user.products.routes')
 
-// app.use('/api-docs', 
-//   swaggerUi.serve,
-//   swaggerUi.setup(swaggerDocument.options)
-// )
+app.use('/api-docs', 
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument.options)
+  )
 
 app.listen(port, ()=>{
   console.log("Server is up");
